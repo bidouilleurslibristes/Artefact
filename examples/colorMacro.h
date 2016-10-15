603813d0c5c0b1e20dce5368113c87427019a17f
Camille-Lihouck <camille.lihouck@gmail.com>
2016-10-15 19:34:46 +0200

    define 8 colors

---
+++ examples/colorMacro.h
@@ -0,0 +1,16 @@
+// Pour importer la librairie sous arduino,
+// il ne suffit pas de mettre #include "colorMacro.h"
+// il faut importer le fichier en allant dans
+// Sketch, Add file et choisir le fichier à importer.
+
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#define red strip.Color(40,01,01)
+#define green strip.Color(01,35,02)
+#define blue strip.Color(02,02,50)
+#define yellow strip.Color(30,28,00)
+#define violet strip.Color(30,00,40)
+#define cyan strip.Color(0,35,25)
+#define orange strip.Color(40,15,00)
+#define red strip.Color(20,20,20)
