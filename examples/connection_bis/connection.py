d90900da343fdf0256690dd62cfe9b6d30b0c559
Matthieu Falce <falce.matthieu@gmail.com>
2016-10-23 17:01:49 +0200

    working init between arduino and python

--- examples/connection_bis/connection.py
+++ examples/connection_bis/connection.py
@@ -1,39 +1,56 @@
 # sudo pip install pyserial
-import serial
 import time
-import binascii
+
+import serial
 
 # ser = serial.Serial('/dev/cu.usbserial-DA00T1YU')
-ser = serial.Serial('/dev/ttyACM1')
+ser = serial.Serial('/dev/ttyACM0', timeout=1)
+ser.flushInput()
+ser.flushOutput()
 
 
-def tryConnection (serialPort):
+def try_connection(serial_port):
     time.sleep(1)
 
+    # Try to read a BONJOUR message
     txt = "toto"
     while not txt.startswith("BONJOUR"):
-        nbBytes = ser.inWaiting()
-        if nbBytes == 0:
+        nb_bytes = serial_port.inWaiting()
+        if nb_bytes == 0:
+            print("no bytes in try connection")
             return False
-        
-        txt = ser.readline().decode("ascii").strip()
 
+        txt = serial_port.readline().decode("ascii").strip()
+
+    # Get the arduino unique id
     txt = txt.split(' ')[1]
-    while ser.inWaiting() != 0:
-        ser.read(ser.inWaiting())
-        
-    print (txt)
-    ser.flush()
-    ser.write(txt.encode())
-    ser.flush()
+    while serial_port.inWaiting() != 0:
+        serial_port.read(serial_port.inWaiting())
 
-    #time.sleep(1)
-    
-    #print(ser.read(ser.inWaiting()).decode("ascii"))
-    
-    
+    # Send back the response for connection
+    print ("from arduino : ", txt)
+    serial_port.write(txt.encode())
 
-tryConnection(ser)
+    # Wait for connection validation
+    while not serial_port.inWaiting():
+        time.sleep(0.01)
+
+    # Verify the CONNECTED message
+    text = serial_port.readline().decode("ascii").strip()
+    print("response from arduino: ", text)
+    if not text:
+        return False
+    if "CONNECTED" not in text:
+        return False
+    return True
+
+
+connected = False
+while not connected:
+    connected = try_connection(ser)
+    print("connected : ", connected)
+    time.sleep(0.1)
+
 while True:
     if ser.inWaiting() > 0:
         print(ser.readline().decode("ascii").strip())
