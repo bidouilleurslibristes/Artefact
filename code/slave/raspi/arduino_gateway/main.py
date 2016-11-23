c9c6e6adc5e2b01cebdef9d449d9512f21045214
Matthieu Falce <falce.matthieu@gmail.com>
2016-11-23 22:22:17 +0100

    add USB VID:PID for an arduino...

--- code/slave/raspi/arduino_gateway/main.py
+++ code/slave/raspi/arduino_gateway/main.py
@@ -36,7 +36,7 @@ nc.start()
 
 
 def _main():
-    serial_ids = ["2a03:0043", "2341:0043"]
+    serial_ids = ["2a03:0043", "2341:0043", "2341:0243"]
     ports = list(list_devices_connected(serial_ids))
     for port in ports:
         if port in connected_devices:
