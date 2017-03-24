8c0e45ce3ce0746c71cd0d26e7f41fd6c3af0dee
yoann-dufresne <yoann.dufresne0@gmail.com>
2017-03-24 19:41:39 +0100

    Reset button

--- slave/arduino/reset_button/reset_button.ino
+++ slave/arduino/reset_button/reset_button.ino
@@ -12,60 +12,24 @@
 
 // Functions definitions :
 void connection ();
+void readInput();
 
-// LED buttons
-int pinRuban = 6;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, pinRuban, NEO_GRB);
 
-uint32_t colors[9];
-
-void initColors(){
-  colors[0] = strip.Color(00, 00, 00);// noir
-  colors[1] = strip.Color(40, 01, 01);// rouge ok
-  colors[2] = strip.Color(01, 35, 02);// vert ok
-  colors[3] = strip.Color(02, 02, 50);// bleu ok
-  colors[4] = strip.Color(30, 28, 0);// jaune ok
-  colors[5] = strip.Color(30, 0, 40);// mauve ok
-  colors[6] = strip.Color(0, 35, 25);//turquoise ok
-  colors[7] = strip.Color(40,15,0);// orange ok
-  colors[8] = strip.Color(20,20,20);// blanc ok
-}
-
-// BUTTONS : 8 + swag
-int buttons[9];
-char buttonsStatus[9];
-int swagLedPin = 2;
-
-void initButtons() {
-  buttons[0] = A0;
-  buttons[1] = A1;
-  buttons[2] = A2;
-  buttons[3] = A3;
-  buttons[4] = A4;
-  buttons[5] = A5;
-  buttons[6] = 8;
-  buttons[7] = 9;
-  buttons[8] = 10;
-}
+int resetButton = 2;
+char status;
 
 
 long last_ping = 0;
-int button1_pressed = 0;
-
 BOOL connected = FALSE;
 
 char ledPin = LED_BUILTIN;
 
-void setupButtons(){
-  for (int i=0 ; i<9 ; i++) {
-    pinMode(buttons[i], INPUT);
-    digitalWrite(buttons[i], INPUT_PULLUP); // connect internal pull-up
-    buttonsStatus[i] = 0;
-  }
+void setupButton(){
+  pinMode(resetButton, INPUT);
+  digitalWrite(resetButton, INPUT_PULLUP); // connect internal pull-up
+  status = 0;
 }
 
-void setLedButtonsColor(String message);
-void setSwagButtonLed(String message);
 
 void setup() {
   Serial.begin(115200);
@@ -74,16 +38,15 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
 
-  strip.begin();
-  initButtons ();
-  initColors ();
-  setupButtons ();
+  setupButton ();
 
   while (42) {
     main_loop();
   }
 }
 
+
+void scanButton();
 
 void main_loop () {
   if (!connected) {
@@ -94,7 +57,7 @@ void main_loop () {
 
     if (Serial.available())
       readInput();
-    scanButtons();
+    scanButton();
   }
 }
 
@@ -140,92 +103,37 @@ void readInput(){
 
   if (ping.equals(str))
     pong();
-  else{
-    parseMessage(str);
-  }
 }
 
-void parseMessage(String message){
-  /*
-  Protocol :
 
-   * 11AC1C2C3C4... : LED STRIP COLORS, message starts with 1 followed by the strip ID, by 32 led colors as defined in color.h => 34 bytes total
-   * 2C1C2C3.... : LED BUTTON COLORS, message starts with 2 followed by 8 bytes for each led RGB color => 9 bytes total
-   * 30/1 : SWAG BUTTON ON, message starts with 3 followed by "0" or "1" (resp off and on) => 2 bytes total
-  */
+void scanButton(){
+  int button_pressed = digitalRead(resetButton) == LOW;
+  bool changed = false;
 
-  char firstChar = message.charAt(0);
-  message.remove(0, 1);
-
-  if(firstChar == '2'){
-    setLedButtonsColor(message);
-  } else if(firstChar == '3'){
-    setSwagButtonLed(message);
-  }
-}
-
-void setLedButtonsColor(String message){
-  for (int i = 0;i<8;i++){
-    char c = message[i];
-    int index = (int)(c - '0');
-    if(index < 0 || index > 8){
-      Serial.println("bad color index ");
-      Serial.print("index mess :");
-      Serial.print(index); Serial.print(' ');
-      Serial.println(message);
-      continue;
-    }
-
-    //buttonColors
-    uint32_t color = colors[index];
-    strip.setPixelColor(i, color);
-  }
-  strip.show();
-}
-
-void setSwagButtonLed(String message){
-  if (message[0] == '0') {
-    digitalWrite(swagLedPin, LOW);
-  } else if (message[0] == '1') {
-    digitalWrite(swagLedPin, HIGH);
-  }
-}
-
-void scanButtons(){
-  bool meta_changed = false;
-
-  for (int i=0 ; i<9 ; i++) {
-    int button_pressed = digitalRead(buttons[i]) == LOW;
-    bool changed = false;
-
-    if(button_pressed && buttonsStatus[i] == 0) {
-      changed = true;
-      buttonsStatus[i] = 1;
-    }
-
-    if(!button_pressed && buttonsStatus[i] == 1) {
-      changed = true;
-      buttonsStatus[i] = 0;
-    }
-
-    if (changed) {
-      meta_changed = true;
-      Serial.print("button-");
-      Serial.print(i);
-      Serial.print("-");
-      if (button_pressed) {
-        Serial.print("DOWN");
-      } else {
-        Serial.print("UP");
-      }
-      Serial.println();
-    }
+  if(button_pressed && status == 0) {
+    changed = true;
+    status = 1;
   }
 
-  if (meta_changed)
+  if(!button_pressed && status == 1) {
+    changed = true;
+    status = 0;
+  }
+
+  if (changed) {
+    Serial.print("button-");
+    Serial.print("0");
+    Serial.print("-");
+    if (button_pressed) {
+      Serial.print("DOWN");
+    } else {
+      Serial.print("UP");
+    }
+    Serial.println();
     delay(30);
+  }
 
-   delay(5);
+  delay(5);
 }
 
 
