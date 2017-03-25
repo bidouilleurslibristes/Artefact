51fee1e9bca62850d69a044db0d085320a7cc4bc
Pierre Marijon <pierre@marijon.fr>
2017-03-25 23:42:27 +0100

    replace vlc call pydub

--- slave/raspi/sound/__init__.py
+++ slave/raspi/sound/__init__.py
@@ -1,64 +1,106 @@
-from subprocess import Popen
-import time
+
 import logging
+import threading
+
+from pydub import AudioSegment
+from pydub.playback import play
 
 logger = logging.getLogger("root")
+
+name2filename = {"atmosphere":
+                 "/home/pi/ZooMachine-3/slave/raspi/sound/atmosphere.mp3", 
+                 "validation":
+                 "/home/pi/ZooMachine-3/slave/raspi/sound/validation.mp3"
+                 }
+
+class Play(threading.Thread):
+    def __init__(self, sound):
+        super(Play, self).__init__()
+        self.running = True
+        self._sound = sound
+
+    def run(self):
+        play(self._sound)
+        print("is end")
+        self.running = False
+
+    def stop(self):
+        self.running = False
 
 class Manager:
 
     def __init__(self):
-        self._filename2process = {}
-        self._name2filename = {"atmosphere": "/home/pi/ZooMachine-3/slave/raspi/sound/atmosphere.mp3",
-                               "validation": "/home/pi/ZooMachine-3/slave/raspi/sound/validation.mp3"
-                               }
+        self._name2sound = {name: AudioSegment.from_mp3(filename) for 
+                            name, filename in name2filename.items()}  
+        self._name2process = {}
 
     def is_ended(self, name):
-        if name not in self._name2filename:
-            logger.error("Unknow sound name {} ".format(name))
+        if not self._is_avaible(name):
             return True
-        return self._filename2process[self._name2filename[name]].poll() is None
+        if name not in self._name2process:
+            return True
+
+        return not self._name2process[name].is_alive()
 
     def restart_if_ended(self, name):
+        if not self._is_avaible(name):
+            return
+        
         if self.is_ended(name):
+            print("Process is ended")
             self.play(name)
+        print("Process isn't ended")
 
     def play(self, name):
-         if name not in self._name2filename:
-            logger.error("Unknow sound name {} ".format(name))
+        if not self._is_avaible(name):
             return
-        self._play(self._name2filename[name])
 
-    def _play(self, filename):
-        if filename in self._filename2process:
-            self._stop(filename)
-        try:
-            args = ["cvlc", "--play-and-exit", filename]
-            self._filename2process[filename] = Popen(args)
-        except Exception as e:
-            logger.exception(e)
+        if name in self._name2process:
+            self._name2process[name].stop()
+        
+        self._name2process[name] = Play(self._name2sound[name])
+        self._name2process[name].start()
 
     def stop(self, name = None):
-        if name not in self._name2filename:
-            logger.error("Unknow sound name {} ".format(name))
-            return
-        self._stop(self._name2filename[name])
-
-    def _stop(self, filename = None):
-        if not filename:
+        if not name:
             #stop all
-            for _, process in self._filename2process.items():
-                process.kill()
+            for _, process in self._name2process.items():
+                process.stop()
 
-            self._filename2process = {}
+            self._name2process = {}
         else:
-            if filename in self._filename2process:
-                self._filename2process[filename].kill()
-                self._filename2process.pop(filename)
+            if not self._is_avaible(name):
+                return
+            if name in self._name2process:
+                self._name2process[name].stop()
+                self._name2process.pop(name)
+
+    def _is_avaible(self, name):
+        if name not in self._name2sound:
+            logger.error("Unknow sound name {} ".format(name))
+            return False
+        return True
 
 if __name__ == "__main__":
+
+    import time
+    name2filename = {
+        "atmosphere": "atmosphere.mp3",
+        "validation": "validation.mp3"
+    }
     m = Manager()
-    m.play("sample.mp3")
-    time.sleep(10)
-    m.play("sample.mp3")
-    time.sleep(10)
-    m.stop()
+
+    # 4 repetition of validation separated by 1 second
+    for _ in range(4):
+        m.restart_if_ended("validation")
+        time.sleep(1)
+
+    # run atmosphere
+    # 4 repetition of validation separated by 1 second
+    # stop atmosphere
+    m.play("atmosphere")
+    for _ in range(4):
+        m.restart_if_ended("validation")
+        time.sleep(1)
+    m.stop("atmosphere")
+
